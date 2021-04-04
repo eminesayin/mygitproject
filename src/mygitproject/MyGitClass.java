8b3fb0517712250ed6b83a796f37d248c5de8ecc
eminesayin <81859198+eminesayin@users.noreply.github.com>
2021-04-04 19:43:47 +0300

    Update MyGitClass.java

--- src/mygitproject/MyGitClass.java
+++ src/mygitproject/MyGitClass.java
@@ -5,7 +5,7 @@ public class MyGitClass {
 	public static void main(String[] args) {
 	System.out.println("Versiyon 1");
 	System.out.println("uzak sunucudan değişiklik");
-
+         System.out.println("versiyon 2");
 	}
 
 }
