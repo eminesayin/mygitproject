6d69fcec923555a2a69bdf6cfdb140520fbff8ae
eminesayin <eminesayin74@gmail.com>
2021-04-11 15:53:22 +0300

    denememesaji

---
+++ src/mygitproject/deneme.java
@@ -0,0 +1,11 @@
+package mygitproject;
+
+public class deneme {
+
+	public static void main(String[] args) {
+		System.out.println("javayiogrendim");
+		System.out.println("encokesrayiseviyorum");
+		
+	}
+
+}
