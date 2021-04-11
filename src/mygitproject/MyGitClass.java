8240098b822192be2468b59ae9eaf3f113cc6a35
eminesayin <eminesayin74@gmail.com>
2021-04-11 15:45:33 +0300

    yenimesaj

--- src/mygitproject/MyGitClass.java
+++ src/mygitproject/MyGitClass.java
@@ -3,9 +3,19 @@ package mygitproject;
 public class MyGitClass {
 
 	public static void main(String[] args) {
-	System.out.println("Versiyon 1");
-	System.out.println("uzak sunucudan değişiklik");
-         System.out.println("versiyon 2");
+		System.out.println("Version 1");
+		System.out.println("My second commit");
+
+		System.out.println("My third commit");
+
+		System.out.println("git hup 16 satır");
+
+		System.out.println("My third commit");
+		System.out.println("My third commit");
+		System.out.println("My third commit");
+		System.out.println("git hup 12 satır");
+		System.out.println("git hup 13 satır");
+
 	}
 
 }
