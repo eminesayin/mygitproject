df8a899f47636778bf6242be178af3f980fec226
eminesayin <81859198+eminesayin@users.noreply.github.com>
2021-04-11 15:56:04 +0300

    Update deneme.java

--- src/mygitproject/deneme.java
+++ src/mygitproject/deneme.java
@@ -5,6 +5,8 @@ public class deneme {
 	public static void main(String[] args) {
 		System.out.println("javayiogrendim");
 		System.out.println("encokesrayiseviyorum");
+		System.out.println("vehriyidesevmiyorumdegil");
+		
 		
 	}
 
